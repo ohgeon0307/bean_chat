042b496ff4117e78b6fb810c524ecf36cb928418
Csytem404 <ahn03777@naver.com>
2023-11-01 13:41:23 +0900

    DTO작성-안기현

--- bean_chat/src/main/java/app/dto/ChatDto.java
+++ bean_chat/src/main/java/app/dto/ChatDto.java
@@ -1,5 +1,68 @@
 package app.dto;
 
 public class ChatDto {
-
+	private int cidx;
+	private String cFrom;
+	private String cTo;
+	private String Ccontents;
+	private String Ctime;
+	private String delyn;
+	private int Uidx;
+	private int Ridx;
+	
+	public int getCidx() {
+		return cidx;
+	}
+	public void setCidx(int cidx) {
+		this.cidx = cidx;
+	}
+	public String getcFrom() {
+		return cFrom;
+	}
+	public void setcFrom(String cFrom) {
+		this.cFrom = cFrom;
+	}
+	public String getcTo() {
+		return cTo;
+	}
+	public void setcTo(String cTo) {
+		this.cTo = cTo;
+	}
+	public String getCcontents() {
+		return Ccontents;
+	}
+	public void setCcontents(String ccontents) {
+		Ccontents = ccontents;
+	}
+	public String getCtime() {
+		return Ctime;
+	}
+	public void setCtime(String ctime) {
+		Ctime = ctime;
+	}
+	public String getDelyn() {
+		return delyn;
+	}
+	public void setDelyn(String delyn) {
+		this.delyn = delyn;
+	}
+	public int getUidx() {
+		return Uidx;
+	}
+	public void setUidx(int uidx) {
+		Uidx = uidx;
+	}
+	public int getRidx() {
+		return Ridx;
+	}
+	public void setRidx(int ridx) {
+		Ridx = ridx;
+	}
+	
+	
+	
+	
+	
+			
+		
 }
