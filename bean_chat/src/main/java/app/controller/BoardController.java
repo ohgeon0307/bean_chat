0aa619c7b80bc6db88c9a4d4a0cf9348938f036d
linchpin <ogeon135@gmail.com>
2023-11-02 12:50:24 +0900

    FrontController / BoardContoller / BoardDao / BoardDto / 코드 작성 및 index.jsp 링크 추가 및 jstl / standard.jar(jstl구문을 사용하기 위한 라이브러리)추가 -건-

--- bean_chat/src/main/java/app/controller/BoardController.java
+++ bean_chat/src/main/java/app/controller/BoardController.java
@@ -1,5 +1,39 @@
 package app.controller;
 
-public class BoardController {
+import java.io.IOException;
+import java.util.ArrayList;
 
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import app.dao.BoardDao;
+import app.dto.BoardDto;
+
+@WebServlet("/BoardController")
+public class BoardController extends HttpServlet{
+private static final long serialVersionUID = 1L;
+	
+	private String location;
+	public BoardController(String location) {
+		this.location = location;		
+	}
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		if(location.equals("boardList.do")) {
+			BoardDao bdao = new BoardDao();
+			ArrayList<BoardDto> alist = bdao.boardSelectAll();
+			
+			request.setAttribute("alist", alist);
+			
+			String path ="/board/board_list.jsp";
+			
+			RequestDispatcher rd = request.getRequestDispatcher(path);
+			rd.forward(request, response);
+		}
+	
+	}
 }
--- bean_chat/src/main/java/app/controller/FrontController.java
+++ bean_chat/src/main/java/app/controller/FrontController.java
@@ -16,32 +16,29 @@ public class FrontController extends HttpServlet {
 		request.setCharacterEncoding("UTF-8");
 		response.setContentType("text/html;charset=UTF-8");
 		
-		//
+
 		String url = request.getRequestURI();	
 		int length = request.getContextPath().length();
 		String command = url.substring(length);	
-		//jfyujfuyruru
-		
-		
-		
-		//   /member/memberList.do   
-		//   /board/boardList.do
 		
 		String[] ary = command.split("/");
 		String str = ary[1];
 		String location = ary[2];
-		//�삤�젏萸�
-		//teststestsdf
-		//오점뭐sdf
-		//sdfsdfsdfsfdsdfsdfsdfssdf
 		
+		//Usercontroller 분기
 		if (str.equals("user")) {
 			UserController uc = new UserController(location);
 			uc.doGet(request, response);
 			
+		//MypageController 분기
 		}else if(str.equals("mypage")) {
 			MypageController mc = new MypageController(location);
-			mc.doGet(request, response);		
+			mc.doGet(request, response);
+		
+		//BoardController 분기
+		}else if(str.equals("board")) {
+			BoardController bc = new BoardController(location);
+			bc.doGet(request, response);
 		}
 		
 	}
