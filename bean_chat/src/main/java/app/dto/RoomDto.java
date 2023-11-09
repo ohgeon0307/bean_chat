5bf8c05edcac27ec121b3934e0ef511c18cb3230
Csytem404 <ahn03777@naver.com>
2023-11-09 14:12:28 +0900

    챗디에이오 계속 수정중 컨텍스트 xml생성 test.jsp생성 생성이유 컨텍스트 xml이 잘 동작되는지 확인하기위함 룸디티오 생성 -기현-

--- bean_chat/src/main/java/app/dto/RoomDto.java
+++ bean_chat/src/main/java/app/dto/RoomDto.java
@@ -2,4 +2,34 @@ package app.dto;
 
 public class RoomDto {
 
+	public int getRidx() {
+		return Ridx;
+	}
+	public void setRidx(int ridx) {
+		Ridx = ridx;
+	}
+	public String getEntry() {
+		return entry;
+	}
+	public void setEntry(String entry) {
+		this.entry = entry;
+	}
+	public String getrName() {
+		return rName;
+	}
+	public void setrName(String rName) {
+		this.rName = rName;
+	}
+	public int getUidx() {
+		return Uidx;
+	}
+	public void setUidx(int uidx) {
+		Uidx = uidx;
+	}
+	private int Ridx;
+	private String entry;
+	private String rName;
+	private int Uidx;
+	
+	
 }
