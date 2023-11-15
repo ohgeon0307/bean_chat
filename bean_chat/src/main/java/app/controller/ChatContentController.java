fc626f683a68ecb0fc668ff78b0e0f3cc3fd1389
linchpin <ogeon135@gmail.com>
2023-11-15 10:48:59 +0900

    conflicts error update (충돌에러수정) -건

--- bean_chat/src/main/java/app/controller/ChatContentController.java
+++ bean_chat/src/main/java/app/controller/ChatContentController.java
@@ -16,101 +16,82 @@ import app.dto.ChatDto;
 
 @WebServlet("/ChatContentController")
 public class ChatContentController extends HttpServlet {
-	private static final long serialVersionUID = 1L;
-	private String location;
+   private static final long serialVersionUID = 1L;
+   private String location;
 
-	public ChatContentController(String location) {
-		this.location = location;
-	}
+   public ChatContentController(String location) {
+      this.location = location;
+   }
 
-	protected void doGet(HttpServletRequest request, HttpServletResponse response)
-<<<<<<< HEAD
-			throws ServletException, IOException 
-			{
-		
-		 if (location.equals("chat_group.do")) 
-		 {
-			
-=======
-			throws ServletException, IOException {
-		if (location.equals("chatList.do")){
-			String path = "/chat/chat_list.jsp";
-			// 화면용도의 주소는 포워드로 토스해서 해당 찐주소로 보낸다
-			RequestDispatcher rd = request.getRequestDispatcher(path);
-			rd.forward(request, response);
-		}else if (location.equals("chat_one.do")) {
-			request.setCharacterEncoding("UTF-8");
-			response.setContentType("text/html;charset=UTF-8");
->>>>>>> branch 'master' of https://github.com/ohgeon0307/bean_chat.git
-			String cFrom = request.getParameter("cFrom");
-			String cTo = request.getParameter("cTo");
-			String listType = request.getParameter("listType");
-			
-			if (cFrom == null || cFrom.equals("") || cTo == null || cTo.equals("") || listType == null
-					|| listType.equals("")){
-				response.getWriter().write("0");
-			}else if(listType.equals("ten")) response.getWriter().write(getTen(URLDecoder.decode( cFrom,"UTF-8"),URLDecoder.decode(cTo,"UTF-8"))); 
-			else {
-				try {
-					
-					response.getWriter().write(getID(URLDecoder.decode( cFrom,"UTF-8"),URLDecoder.decode(cTo,"UTF-8"), listType));
-					}catch(Exception e)
-				{
-					response.getWriter().write("");
-				}	
-			
-				}
-<<<<<<< HEAD
-			String path = "/chat/chat_group.jsp";
-			// 화면용도의 주소는 포워드로 토스해서 해당 찐주소로 보낸다
-			RequestDispatcher rd = request.getRequestDispatcher(path);
-			rd.forward(request, response);
-=======
-			
-			//	System.out.println("값이담기나?"+cTo);
-			}
->>>>>>> branch 'master' of https://github.com/ohgeon0307/bean_chat.git
-		}
-			}
-		
-	public String getTen(String cFrom , String cTo) 
-	{
-		StringBuffer result = new StringBuffer("");
-		result.append("{\"result\":[");
-		ChatDao chatDao =new ChatDao();
-		ArrayList<ChatDto> chatList = chatDao.getChatListByRecent(cFrom, cTo, 10);
-		if(chatList.size() == 0) return "";
-		for(int i = 0; i <  chatList.size(); i++ ) 
-		{
-			result.append("[{\"value\":\""+chatList.get(i).getcFrom()+"\"},");
-			result.append("{\"value\":\""+chatList.get(i).getcTo()+"\"},");
-			result.append("{\"value\":\""+chatList.get(i).getcContents()+"\"},");
-			result.append("{\"value\":\""+chatList.get(i).getcTime()+"\"}]");
-			if(i !=chatList.size() -1) result.append(",");
-			
-		}
-		result.append("],\"last\":\""+chatList.get(chatList.size()-1).getCidx()+"\"}");
-		return result.toString();
-	}
-	public String getID(String cFrom , String cTo, String cidx) {
-		StringBuffer result = new StringBuffer("");
-		result.append("{\"result\":[");
-		ChatDao chatDao =new ChatDao();
-		ArrayList<ChatDto> chatList = chatDao.getChatListByID(cFrom, cTo, cidx);
-		if(chatList.size() == 0) return "";
-		for(int i = 0; i <  chatList.size(); i++ ) {
-			result.append("[{\"value\":\""+chatList.get(i).getcFrom()+"\"},");
-			result.append("{\"value\":\""+chatList.get(i).getcTo()+"\"},");
-			result.append("{\"value\":\""+chatList.get(i).getcContents()+"\"},");
-			result.append("{\"value\":\""+chatList.get(i).getcTime()+"\"}]");
-			if(i !=chatList.size() -1) result.append(",");
-			
-		}
-		result.append("],\"last\":\""+chatList.get(chatList.size()-1).getCidx()+"\"}");
-		return result.toString();
-		
-		//
-	}
-	
-	
+   protected void doGet(HttpServletRequest request, HttpServletResponse response)
+         throws ServletException, IOException {
+      if (location.equals("chatList.do")){
+         String path = "/chat/chat_list.jsp";
+         // 화면용도의 주소는 포워드로 토스해서 해당 찐주소로 보낸다
+         RequestDispatcher rd = request.getRequestDispatcher(path);
+         rd.forward(request, response);
+      }else if (location.equals("chat_one.do")) {
+         request.setCharacterEncoding("UTF-8");
+         response.setContentType("text/html;charset=UTF-8");
+         String cFrom = request.getParameter("cFrom");
+         String cTo = request.getParameter("cTo");
+         String listType = request.getParameter("listType");
+         
+         if (cFrom == null || cFrom.equals("") || cTo == null || cTo.equals("") || listType == null
+               || listType.equals("")){
+            response.getWriter().write("0");
+         }else if(listType.equals("ten")) response.getWriter().write(getTen(URLDecoder.decode( cFrom,"UTF-8"),URLDecoder.decode(cTo,"UTF-8"))); 
+         else {
+            try {
+               
+               response.getWriter().write(getID(URLDecoder.decode( cFrom,"UTF-8"),URLDecoder.decode(cTo,"UTF-8"), listType));
+            }catch(Exception e) {
+               response.getWriter().write("");
+            }
+         
+         //   System.out.println("값이담기나?"+cTo);
+         }
+      }
+         
+         }
+      
+   public String getTen(String cFrom , String cTo) {
+      StringBuffer result = new StringBuffer("");
+      result.append("{\"result\":[");
+      ChatDao chatDao =new ChatDao();
+      ArrayList<ChatDto> chatList = chatDao.getChatListByRecent(cFrom, cTo, 10);
+      if(chatList.size() == 0) return "";
+      for(int i = 0; i <  chatList.size(); i++ ) {
+         result.append("[{\"value\":\""+chatList.get(i).getcFrom()+"\"},");
+         result.append("{\"value\":\""+chatList.get(i).getcTo()+"\"},");
+         result.append("{\"value\":\""+chatList.get(i).getcContents()+"\"},");
+         result.append("{\"value\":\""+chatList.get(i).getcTime()+"\"}]");
+         if(i !=chatList.size() -1) result.append(",");
+         
+         
+      }
+      result.append("],\"last\":\""+chatList.get(chatList.size()-1).getCidx()+"\"}");
+      return result.toString();
+   }
+   public String getID(String cFrom , String cTo, String cidx) {
+      StringBuffer result = new StringBuffer("");
+      result.append("{\"result\":[");
+      ChatDao chatDao =new ChatDao();
+      ArrayList<ChatDto> chatList = chatDao.getChatListByID(cFrom, cTo, cidx);
+      if(chatList.size() == 0) return "";
+      for(int i = 0; i <  chatList.size(); i++ ) {
+         result.append("[{\"value\":\""+chatList.get(i).getcFrom()+"\"},");
+         result.append("{\"value\":\""+chatList.get(i).getcTo()+"\"},");
+         result.append("{\"value\":\""+chatList.get(i).getcContents()+"\"},");
+         result.append("{\"value\":\""+chatList.get(i).getcTime()+"\"}]");
+         if(i !=chatList.size() -1) result.append(",");
+         
+      }
+      result.append("],\"last\":\""+chatList.get(chatList.size()-1).getCidx()+"\"}");
+      return result.toString();
+      
+      //
+   }
+   
+   
 }
--- bean_chat/src/main/java/app/controller/ChatController.java
+++ bean_chat/src/main/java/app/controller/ChatController.java
@@ -15,63 +15,55 @@ import app.dao.ChatDao;
 
 @WebServlet("/ChatController")
 public class ChatController extends HttpServlet {
-	private static final long serialVersionUID = 1L;
-	private String location;
+   private static final long serialVersionUID = 1L;
+   private String location;
 
-	public ChatController(String location) {
-		this.location = location;
-	}
+   public ChatController(String location) {
+      this.location = location;
+   }
 
-	protected void doGet(HttpServletRequest request, HttpServletResponse response)
-			throws ServletException, IOException {
-		if (location.equals("chatList.do"))
+   protected void doGet(HttpServletRequest request, HttpServletResponse response)
+         throws ServletException, IOException {
+      if (location.equals("chatList.do"))
 
-		{
-			String path = "/chat/chat_list.jsp";
-			// 화면용도의 주소는 포워드로 토스해서 해당 찐주소로 보낸다
-			RequestDispatcher rd = request.getRequestDispatcher(path);
-			rd.forward(request, response);
-		}
+      {
+         String path = "/chat/chat_list.jsp";
+         // 화면용도의 주소는 포워드로 토스해서 해당 찐주소로 보낸다
+         RequestDispatcher rd = request.getRequestDispatcher(path);
+         rd.forward(request, response);
+      }
 
-<<<<<<< HEAD
-		else if (location.equals("chat_group.do")) {
+      else if (location.equals("chat_one.do")) {
+         
+         
+         request.setCharacterEncoding("UTF-8");
+         response.setContentType("text/html;charset=UTF-8");
+         
+         String cFrom = request.getParameter("cFrom");
+         String cTo = request.getParameter("cTo");
+         String cContents = request.getParameter("cContents");
+         
+          HttpSession session = request.getSession();
+          session.setAttribute("cTo", cTo);
+          
+         if (cFrom == null || cFrom.equals("") || cTo == null || cTo.equals("") || cContents == null
+               || cContents.equals("")) {
+            response.getWriter().write("0");
 
-=======
-		else if (location.equals("chat_one.do")) {
-			
-			
-			request.setCharacterEncoding("UTF-8");
-			response.setContentType("text/html;charset=UTF-8");
-<<<<<<< HEAD
->>>>>>> branch 'master' of https://github.com/ohgeon0307/bean_chat.git
-=======
-			
->>>>>>> branch 'master' of https://github.com/ohgeon0307/bean_chat.git
-			String cFrom = request.getParameter("cFrom");
-			String cTo = request.getParameter("cTo");
-			String cContents = request.getParameter("cContents");
-			
-		    HttpSession session = request.getSession();
-		    session.setAttribute("cTo", cTo);
-		    
-			if (cFrom == null || cFrom.equals("") || cTo == null || cTo.equals("") || cContents == null
-					|| cContents.equals("")) {
-				response.getWriter().write("0");
+         } else  {
+            cFrom = URLDecoder.decode(cFrom, "UTF-8");
+            cTo = URLDecoder.decode(cTo, "UTF-8");
+            cContents = URLDecoder.decode(cContents, "UTF-8");
+            response.getWriter().write(new ChatDao().submit(cFrom, cTo, cContents) + "");
 
-			} else  {
-				cFrom = URLDecoder.decode(cFrom, "UTF-8");
-				cTo = URLDecoder.decode(cTo, "UTF-8");
-				cContents = URLDecoder.decode(cContents, "UTF-8");
-				response.getWriter().write(new ChatDao().submit(cFrom, cTo, cContents) + "");
-
-			}
-			
-			String path = "/chat/chat_one.jsp";
-			// 화면용도의 주소는 포워드로 토스해서 해당 찐주소로 보낸다
-			RequestDispatcher rd = request.getRequestDispatcher(path);
-			rd.forward(request, response);
+         }
+         
+         String path = "/chat/chat_one.jsp";
+         // 화면용도의 주소는 포워드로 토스해서 해당 찐주소로 보낸다
+         RequestDispatcher rd = request.getRequestDispatcher(path);
+         rd.forward(request, response);
 //
-		}
-	}
+      }
+   }
 
-}
+} 
