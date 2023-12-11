d40add687da27a8c67064a09c68cfc13b457da50
Sehyeony <tpgus103@naver.com>
2023-12-11 16:44:46 +0900

    +세현+ 공지사항 컨트롤러,dao 작성

---
+++ bean_chat/src/main/java/app/controller/NoticeController.java
@@ -0,0 +1,241 @@
+package app.controller;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.ArrayList;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import app.dao.NoticeDao;
+import app.dao.UserDao;
+import app.dto.BoardDto;
+import app.dto.UserDto;
+
+@WebServlet("/NoticeController")
+public class NoticeController extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	private String location;
+
+	public NoticeController(String location) {
+		this.location = location;
+	}
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		if (location.equals("boardList.do")) {
+			NoticeDao ndao = new NoticeDao();
+			ArrayList<BoardDto> alist = ndao.boardSelectAll();
+
+			request.setAttribute("alist", alist);
+
+			String path = "/notice/notice_list.jsp";
+
+			RequestDispatcher rd = request.getRequestDispatcher(path);
+			rd.forward(request, response);
+
+		} else if (location.equals("noticeWrite.do")) {
+			HttpSession session = request.getSession();
+			int uidx = 0; // 초기값 설정
+
+			if (session.getAttribute("uidx") != null) {
+				uidx = (Integer) session.getAttribute("uidx");
+			} else {
+				// 로그인되지 않은 상태이므로 로그인 페이지로 이동
+				response.setContentType("text/html;charset=UTF-8");
+				PrintWriter out = response.getWriter();
+				out.println("<script>alert('로그인이 필요합니다.'); location.href='" + request.getContextPath()
+						+ "/user/user_login.jsp';</script>");
+				out.close();
+				return;
+			}
+
+			UserDao udao = new UserDao();
+			UserDto udto = udao.UserSelectOne(uidx);
+
+			if (udto != null) {
+				System.out.println("udto.userNickname: " + udto.getUserNickname());
+				request.setAttribute("udto", udto);
+			} else {
+				System.out.println("udto is null or empty.");
+			}
+
+			String path = "/notice/notice_write.jsp";
+
+			RequestDispatcher rd = request.getRequestDispatcher(path);
+			rd.forward(request, response);
+		} else if (location.equals("noticeWriteAction.do")) {
+
+			HttpSession session = request.getSession();
+			int uidx = (Integer) session.getAttribute("uidx");
+
+			String subject = request.getParameter("subject");
+			System.out.println("subject : " + subject);
+			String contents = request.getParameter("contents");
+			System.out.println("contents : " + contents);
+			String writer = request.getParameter("writer");
+			System.out.println("writer : " + writer);
+
+			BoardDto bdto = new BoardDto();
+			bdto.setSubject(subject);
+			bdto.setContents(contents);
+			bdto.setWriter(writer);
+			// bdto.setFilename(fileName);
+			bdto.setUidx(uidx);
+
+			NoticeDao ndao = new NoticeDao();
+			int value = ndao.boardInsert(bdto);
+
+			if (value == 0) {
+				String path = request.getContextPath() + "/board/boardWrite.do";
+				response.sendRedirect(path);
+			} else {
+				String path = request.getContextPath() + "/board/boardList.do";
+				response.sendRedirect(path);
+			}
+
+		} else if (location.equals("boardContents.do")) {
+			String bidx = request.getParameter("bidx");
+			int bidx_int = Integer.parseInt(bidx);
+
+			NoticeDao ndao = new NoticeDao();
+			BoardDto bdto = ndao.boardSelectOne(bidx_int);
+
+			request.setAttribute("bdto", bdto);
+
+			String path = "/notice/notice_contents.jsp";
+
+			RequestDispatcher rd = request.getRequestDispatcher(path);
+			rd.forward(request, response);
+
+		} else if (location.equals("noticeModify.do")) {
+			HttpSession session = request.getSession();
+			int uidx = 0;
+
+			if (session.getAttribute("uidx") != null) {
+				uidx = (Integer) session.getAttribute("uidx");
+			} else {
+				// 로그인되지 않은 상태이므로 로그인이 필요합니다. 알림 메시지 출력 및 로그인 페이지로 이동
+				response.setContentType("text/html;charset=UTF-8");
+				response.setContentType("text/html;charset=UTF-8");
+				PrintWriter out = response.getWriter();
+				out.println("<script>alert('로그인이 필요합니다.'); location.href='" + request.getContextPath()
+						+ "/user/user_login.jsp';</script>");
+				out.close();
+				return;
+			}
+
+			// 권한 확인
+			String bidx = request.getParameter("bidx");
+			int bidx_int = Integer.parseInt(bidx);
+
+			NoticeDao nd = new NoticeDao();
+			String authorUidx = nd.getAuthorUidx(bidx_int); // 해당 글의 작성자 Uidx를 가져옴
+
+			if (uidx != 0 && uidx == Integer.parseInt(authorUidx)) {
+				// 권한이 있는 경우에만 수정 페이지로 이동
+				UserDao udao = new UserDao();
+				UserDto udto = udao.UserSelectOne(uidx);
+
+				if (udto != null) {
+					request.setAttribute("udto", udto);
+				} else {
+					response.sendRedirect(request.getContextPath() + "/board/errorPage.jsp");
+				}
+
+				BoardDto bdto = nd.boardSelectOne(bidx_int);
+				request.setAttribute("bdto", bdto);
+
+				String path = "/notice/notice_modify.jsp";
+				RequestDispatcher rd = request.getRequestDispatcher(path);
+				rd.forward(request, response);
+			} else {
+				// 권한이 없는 경우에 대한 처리
+				// 예: 권한이 없다는 메시지를 출력하거나 다른 페이지로 리다이렉트
+				response.sendRedirect(request.getContextPath() + "/board/errorPage.jsp");
+			}
+
+		} else if (location.equals("noticeModifyAction.do")) {
+			String bidx = request.getParameter("bidx");
+			System.out.println("-----------" + bidx);
+			String subject = request.getParameter("subject");
+			String contents = request.getParameter("contents");
+			String writer = request.getParameter("writer");
+
+			BoardDto bdto = new BoardDto();
+			bdto.setSubject(subject);
+			bdto.setContents(contents);
+			bdto.setWriter(writer);
+			bdto.setBidx(Integer.parseInt(bidx));
+
+			// 수정 처리 부분
+			HttpSession session = request.getSession();
+			int uidx = (Integer) session.getAttribute("uidx");
+
+			NoticeDao nd = new NoticeDao();
+			String authorUidx = nd.getAuthorUidx(Integer.parseInt(bidx)); // 해당 글의 작성자 Uidx를 가져옴
+
+			if (uidx != 0 && uidx == Integer.parseInt(authorUidx)) {
+				// 현재 로그인된 사용자가 글의 작성자와 일치하는 경우에만 수정 수행
+				int value = nd.boardModify(bdto);
+
+				if (value != 0) {
+					String path = request.getContextPath() + "/notice/noticeContents.do?bidx=" + bidx;
+					response.sendRedirect(path);
+				} else {
+					// 수정 실패 시 처리 (예: 에러 페이지로 리다이렉트)
+					response.sendRedirect(request.getContextPath() + "/board/errorPage.jsp");
+				}
+			} else {
+				// 수정 권한이 없는 경우에 대한 처리
+				// 예: 권한이 없다는 메시지를 출력하거나 다른 페이지로 리다이렉트
+				response.sendRedirect(request.getContextPath() + "/board/errorPage.jsp");
+			}
+		} else if (location.equals("noticeDeleteAction.do")) {
+			HttpSession session = request.getSession();
+			int uidx = 0; // 초기값 설정
+
+			if (session.getAttribute("uidx") != null) {
+				// 로그인된 사용자의 ID를 가져옴 (세션에서 또는 사용자 인증 시스템에서)
+				uidx = (Integer) session.getAttribute("uidx");
+			} else {
+				// 로그인되지 않은 상태이므로 로그인이 필요합니다. 알림 메시지 출력 및 로그인 페이지로 이동
+				response.setContentType("text/html;charset=UTF-8");
+				PrintWriter out = response.getWriter();
+				out.println("<script>alert('로그인이 필요합니다.'); location.href='" + request.getContextPath()
+						+ "/user/user_login.jsp';</script>");
+				out.close();
+				return;
+			}
+
+			String bidx = request.getParameter("bidx");
+			int bidx_int = Integer.parseInt(bidx);
+
+			NoticeDao ndao = new NoticeDao();
+			String authorId = ndao.getAuthorUidx(bidx_int); // 해당 글의 작성자 ID를 가져옴
+
+			if (uidx != 0 && Integer.toString(uidx).equals(authorId)) {
+				// 현재 로그인된 사용자가 글의 작성자와 일치하는 경우에만 삭제 수행
+				int value = ndao.boardDelete(bidx_int);
+
+				if (value != 0) {
+					String path = request.getContextPath() + "/notice/noticeList.do";
+					response.sendRedirect(path);
+				} else {
+					String path = request.getContextPath() + "/notice/noticeContents.do?bidx=" + bidx;
+					response.sendRedirect(path);
+				}
+			} else {
+				// 삭제 권한이 없는 경우에 대한 처리
+				// 예: 권한이 없다는 메시지를 출력하거나 다른 페이지로 리다이렉트
+				response.sendRedirect(request.getContextPath() + "/board/errorPage.jsp");
+			}
+		}
+	}
+}
---
+++ bean_chat/src/main/java/app/dao/NoticeDao.java
@@ -0,0 +1,199 @@
+package app.dao;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+
+import app.dbconn.DbConn;
+import app.dto.BoardDto;
+
+public class NoticeDao {
+	
+	private Connection conn;
+	private PreparedStatement pstmt;
+	
+	public NoticeDao() {
+		DbConn dbconn = new DbConn();
+		this.conn = dbconn.getConnection();
+	}
+	
+	public ArrayList<BoardDto> boardSelectAll() {
+		
+		ArrayList<BoardDto> alist = new ArrayList<BoardDto>();
+		ResultSet rs = null;
+		
+		String sql = "select bidx, subject,writer,viewcnt,writedate\r\n"
+					+ "from boardtable\r\n"
+					+ "where bDelYn ='N'";
+		
+		try {
+			pstmt = conn.prepareStatement(sql);
+			rs = pstmt.executeQuery();
+	
+		
+		while(rs.next()) {
+			BoardDto bdto = new BoardDto();
+			bdto.setBidx(rs.getInt("bidx"));
+			bdto.setSubject(rs.getString("subject"));
+			bdto.setWriter(rs.getString("writer"));
+			bdto.setViewCnt(rs.getInt("viewCnt"));
+			bdto.setWriteDate(rs.getString("writeDate"));
+			alist.add(bdto);
+		}
+		}catch(Exception e) {
+			e.printStackTrace();
+		}finally {
+			try{
+				rs.close();
+				pstmt.close();
+				//conn.close();
+			}catch(Exception e){
+				e.printStackTrace();
+			}
+		}
+		
+		return alist;
+	}
+	
+	public int boardInsert(BoardDto bdto) {
+		int exec = 0;
+		
+		String sql = "INSERT INTO boardtable(bidx,subject,contents,writer,uidx,filename)\r\n"
+				+ "VALUES(?,?,?,?,?,?)";
+		
+		try{
+			conn.setAutoCommit(false);
+			pstmt = conn.prepareStatement(sql);
+			pstmt.setInt(1, bdto.getBidx());
+			pstmt.setString(2, bdto.getSubject());
+			pstmt.setString(3, bdto.getContents());
+			pstmt.setString(4, bdto.getWriter());
+			pstmt.setInt(5, bdto.getUidx());	
+			pstmt.setString(6, bdto.getFileName());
+			
+			
+			exec = pstmt.executeUpdate();
+			
+			conn.commit();
+			
+			}catch(Exception e){
+				try {
+					conn.rollback();
+				} catch (SQLException e1) {				
+					e1.printStackTrace();
+				}
+				e.printStackTrace();
+			}
+			return exec;	
+		}
+	
+	public BoardDto boardSelectOne(int bidx) {
+		BoardDto bdto = null;
+		String sql = "select * from boardtable where bidx=?";
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
+		
+		try {
+			pstmt = conn.prepareStatement(sql);
+			pstmt.setInt(1, bidx);
+			rs = pstmt.executeQuery();
+			
+			if(rs.next()) {
+				bdto = new BoardDto();
+				bdto.setSubject(rs.getString("subject"));
+				bdto.setContents(rs.getString("contents"));
+				bdto.setWriter(rs.getString("writer"));
+				bdto.setBidx(rs.getInt("bidx"));
+				bdto.setViewCnt(rs.getInt("viewcnt"));
+				bdto.setbList(rs.getString("bList"));
+				bdto.setWriteDate(rs.getString("writedate"));
+			}
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}finally{
+			try{
+				rs.close();
+				pstmt.close();
+				conn.close();
+			}catch(Exception e){
+				e.printStackTrace();
+			}
+		}	
+		
+		return bdto;
+	}
+	
+	public int boardModify(BoardDto bdto) {
+		int exec = 0;
+		String sql = "update boardtable set\r\n"
+				+ "subject = ?, \r\n"
+				+ "contents = ?, \r\n"
+				+ "writer = ?, \r\n"
+				+ "modifydate = now() \r\n"
+				+ "where bidx = ?";
+		
+		try {
+			pstmt = conn.prepareStatement(sql);
+			pstmt.setString(1, bdto.getSubject());
+			pstmt.setString(2, bdto.getContents());
+			pstmt.setString(3, bdto.getWriter());
+			pstmt.setInt(4, bdto.getBidx());
+			
+			exec = pstmt.executeUpdate();
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+		return exec;
+	}
+	
+	public int boardDelete(int bidx) {
+		int exec = 0;
+		
+		String sql = "update boardtable set\r\n"
+				+ "bDelYn = 'Y', \r\n"
+				+ "modifydate = now() \r\n"
+				+ "where bidx = ?";
+		
+		try {
+			pstmt = conn.prepareStatement(sql);
+			pstmt.setInt(1, bidx);
+			
+			exec = pstmt.executeUpdate();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return exec;
+	}
+	
+	 public String getAuthorUidx(int bidx) {
+	        String authorUidx = null;
+
+	        // 해당 글의 작성자 uidx를 가져오는 SQL 쿼리
+	        String sql = "SELECT uidx FROM boardtable WHERE bidx = ?";
+
+	        try {
+	            // 데이터베이스 연결 등의 초기 작업 수행
+
+	            pstmt = conn.prepareStatement(sql);
+	            pstmt.setInt(1, bidx);
+	            ResultSet rs = pstmt.executeQuery();
+
+	            if (rs.next()) {
+	                // 결과에서 작성자 ID를 가져옴
+	            	authorUidx = rs.getString("uidx");
+	            }
+
+	            // 사용한 자원 반환 등의 마무리 작업 수행
+
+	        } catch (SQLException e) {
+	            e.printStackTrace();
+	        }
+
+	        return authorUidx;
+	    }
+	
+	
+	}
